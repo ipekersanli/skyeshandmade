a59852e513e4bc1ce31b54a01e7770dcf6523399
Ipek <ipos888@gmail.com>
2022-03-07 15:09:41 +0300

    ProductInCart class is added with fields

--- products/models.py
+++ products/models.py
@@ -1,9 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Product(models.Model):
     name = models.CharField( max_length=50)
     description =  models.TextField( max_length=150)
     price = models.DecimalField( max_digits=5, decimal_places=2)
     image = models.ImageField(upload_to='uploads/')
+    
+    def __str__(self):
+        return self.name
+
+class ProductsInCart(models.Model):
+    customer =  models.OneToOneField(User, on_delete=models.CASCADE)
+    product =  models.OneToOneField("products.Product", on_delete=models.CASCADE)
+    amount = models.IntegerField()
+
 # Create your models here.
