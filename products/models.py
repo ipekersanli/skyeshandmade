4c72b6008c58ff5ff48dfa3b0738919df93aa4b6
Ipek <ipos888@gmail.com>
2022-03-07 12:47:56 +0300

    fields added to product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Product(models.Model):
+name = models.CharField((""), max_length=50)
+description =  models.TextField((""), max_length=150)
+price = models.DecimalField((""), max_digits=5, decimal_places=2)
+image = models.models.ImageField((""), upload_to=None, height_field=None, width_field=None, max_length=None)
+
 # Create your models here.
