3ad386a7166da3819e720a9cbc8acfce2623c962
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-04-05 01:34:23 +0300

    Iyi geceler :)

--- IpekProject/urls.py
+++ IpekProject/urls.py
@@ -25,6 +25,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.ProductListView.as_view()),
+    path('add-to-cart/<int:product_id>/', views.ProductInCartCreateView.as_view()),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView, DetailView, CreateView
-
+from django.shortcuts import redirect
+from django.views.generic import ListView, DetailView, View
 from products.models import Product, ProductInCart
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
 class ProductListView(ListView):
@@ -9,15 +11,22 @@ class ProductListView(ListView):
 
 
 class ProductInCartListView(ListView):
+    template_name = 'products/products_in_cart.html'
+
     def get_queryset(self):
         ProductInCart.objects.filter(user=self.request.user)
-        template_name = 'products/products_in_cart.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
 
-class ProductInCartCreateView(CreateView):
-    model = Product
-    template_name = "products,products_in_cart_create.html"
+
+@method_decorator(login_required, name='get')
+class ProductInCartCreateView(View):
+    def get(self, request, *args, **kwargs):
+        # product in cart yarat, customer olarak request.user kullanabilirsin
+        # url lere ekledim, add-to-cart/5 e erisilirse id'si 5 olan product, product in cart objesinin,
+        # product'u olacak, yazdigin modelleri unutma bol bol googlla,
+        # bu sayiya kwargs['product_id'] ile erisebilirsin.
+        return redirect('/') # bu da operasyon bitince redirectliyor, tirnak icindeki urlye
