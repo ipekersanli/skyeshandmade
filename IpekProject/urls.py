7d7ce348239e89089c0e9040e4e7e102c85a62ed
ipekersanli <95385327+ipekersanli@users.noreply.github.com>
2022-04-05 00:06:10 +0300

    products page edits

--- IpekProject/urls.py
+++ IpekProject/urls.py
@@ -21,5 +21,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.ProductListView.as_view()),
+    path('products/', views.ProductListView.as_view()),
 ]
--- products/views.py
+++ products/views.py
@@ -10,8 +10,14 @@ class ProductListView(ListView):
 class ProductInCartListView(ListView):
     def get_queryset(self):
         ProductInCart.objects.filter(user=self.request.user)
+        template_name = 'products/products_in_cart.html'
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
+
+class ProductInCartCreateView(CreateView):
+    model = Product
+    template_name = "products,products_in_cart_create.html"
+)
