1f86c16774c98542e526d4c8408d48a2b3e61ca3
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-04-05 01:03:33 +0300

    Add login logout URL's

--- IpekProject/urls.py
+++ IpekProject/urls.py
@@ -19,9 +19,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.ProductListView.as_view()),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
