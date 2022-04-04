e1eeb7f72c9fa4272d909f07ba5a17aa70369959
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-04-05 00:56:30 +0300

    Enable serving uploaded media

--- IpekProject/settings.py
+++ IpekProject/settings.py
@@ -127,6 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = 'uploads/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=150)
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    image = models.ImageField(upload_to='uploads/')
+    image = models.ImageField()
 
     def __str__(self):
         return self.name
